a97a2676144734b9dae8360ed5462376efae25ce
025.030174 <025.030174@C305-3023683>
2026-03-02 10:15:00 -0300

    Nova alteracao

--- desafio_backend_sprint1_Filipe_Menezes/Program.cs
+++ desafio_backend_sprint1_Filipe_Menezes/Program.cs
@@ -34,13 +34,62 @@
             {
                 case 1:
                     Console.WriteLine("Acessando conta corrente...");
+
+
+
+                    Console.WriteLine("Digite o numero da conta");
+                    int NumeroContaC = int.Parse(Console.ReadLine());
+
+                    Console.WriteLine("Digite o nome do titular da conta");
+                    string TitularC = Console.ReadLine();
+
+                    Console.WriteLine("Digite o saldo inicial da conta");
+                    decimal SaldoInicialC = decimal.Parse(Console.ReadLine());
+
+                    
+
                     break;
+
                 case 2:
                     Console.WriteLine("Acessando conta poupança...");
+
+
+
+
+                    Console.WriteLine("Digite o numero da conta");
+                    int NumeroContaP = int.Parse(Console.ReadLine());
+
+                    Console.WriteLine("Digite o nome do titular da conta");
+                    string TitularP = Console.ReadLine();
+
+                    Console.WriteLine("Digite o saldo inicial da conta");
+                    decimal SaldoInicialP = decimal.Parse(Console.ReadLine());
+
+
+
+
                     break;
+
                 case 3:
                     Console.WriteLine("Acessando conta empresarial...");
+
+
+
+
+                    Console.WriteLine("Digite o numero da conta");
+                    int NumeroContaÈ = int.Parse(Console.ReadLine());
+
+                    Console.WriteLine("Digite o nome do titular da conta");
+                    string TitularE = Console.ReadLine();
+
+                    Console.WriteLine("Digite o saldo inicial da conta");
+                    decimal SaldoInicialE = decimal.Parse(Console.ReadLine());
+
+
+
+
                     break;
+
                 case 4:
                     Console.WriteLine("Saindo do programa....");
                     break;
@@ -52,7 +101,7 @@
         }
         ExibirMenu();
 
-
+        
 
 
 
